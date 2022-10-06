a14d6892843c0e9e5495186c4d45d495eb7fbd05
고경훈 <sand9688@naver.com>
2022-10-06 15:13:54 +0900

    prac

--- 04.Express/promis2.js
+++ 04.Express/promis2.js
@@ -1,4 +1,9 @@
 class varClass {
+    constructor(func){
+        this.state = "";
+        this.result = "";
+    }
+
     ret() {
         console.log('ret')
         return new varClass;
@@ -6,11 +11,44 @@ class varClass {
     on() {
         console.log('on');
         return new varClass;
-    }ㄴ
+    }
+    reject(msg) {
+        this.result =msg;
+        this.state = "rejected"
+
+        return this;
+    }
+    resolve(msg) {
+        this.result = msg;
+        this.state = "resolved"
+        return this;
+    }
+    then(func2) {
+        if (this.state == "resolved"){
+            func2(this.result)
+        } 
+        return this;
+    }
+
+    catch(func3){
+        if (this.state == "rejected"){
+            func3(this.result)
+        }
+        return this;
+    }
 };
 
 let tempVar1 = new varClass();
+tempVar1.reject("실패했어요");
+tempVar1
+.then((message)=>{
+    console.log(message);
+})
+.resolve('성공했어요')
+.catch((message)=>{
+    console.log(message);
+})
+.then((message)=>{
+    console.log(message);
+})
 
-let tempVar2 = tempVar1.ret().on();
-
-//
